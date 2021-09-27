1c5a4623d2f0a71eca26f0631d2f2575a0c0813f
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2021-09-26 18:52:52 -0600

    Initial test app completed

---
+++ OWTCNavigator/OWTCNavigatorClasses/OWTCNavigatorClasses/CourseDeadline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWTCNavigatorClasses
+{
+    public class CourseDeadline
+    {
+        private DateTime deadline;
+
+        public CourseDeadline(DateTime deadline)
+        {
+            Deadline = deadline;
+        }
+
+        public DateTime Deadline
+        {
+            get
+            {
+                return deadline;
+            }
+            set
+            {
+                // Time and a half of course starting date plus progress hours
+            }
+        }
+    }
+}
---
+++ OWTCNavigator/OWTCNavigatorClasses/OWTCNavigatorClasses/Schedule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWTCNavigatorClasses
+{
+    public class Schedule
+    {
+        private List<int> scheduleList;
+
+        public Schedule(List<int> scheduleList)
+        {
+            ScheduleList = scheduleList;
+        }
+
+        public List<int> ScheduleList
+        {
+            get
+            {
+                return scheduleList;
+            }
+            set
+            {
+                scheduleList = value;
+            }
+        }
+    }
+}
